dda945c46bc8132a3faf386239a5e5349c92deee
sergey-komar <kursev81@yandex.ru>
2023-04-12 21:52:29 +0300

    desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,68 @@
 $(function () {
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+		slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        dots: true,
+
+    });
 
 })
+
+const modalBtn = document.querySelectorAll('[data-modal]');
+const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('[data-close]');
+const scroll = calcScroll();
+
+
+    modalBtn.forEach(btn =>{
+            btn.addEventListener('click', ()=>{
+                modal.classList.add('show');
+                modal.classList.remove('hide');
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
+            })
+        });
+
+    modalCloseBtn.addEventListener('click', ()=>{
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+    });
+
+    modal.addEventListener('click', (e)=>{
+        if(e.target == modal){
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        }
+    });
+
+    document.addEventListener('keydown', (e)=>{
+        if(e.code == 'Escape' && modal.classList.contains('show')){
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        }
+    })
+
+
+function calcScroll(){
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    
+    return scrollWidth;
+    }
+  
+
